90ad2eb505715f1a920e0e3b2f80f5c08322e3a6
Pete Turnham <73126560+peteturnham@users.noreply.github.com>
2021-04-09 11:59:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,10 +22,6 @@ graph()
 init();
 // if subjectID is updated, re-graph for new subjectID
 function optionChanged() {
-<<<<<<< HEAD
-  Plotly.restyle
-=======
->>>>>>> afb13885d40b279dda53a03dac5bef48df8c184a
   graph();
 }
 // function for generating plots
@@ -36,21 +32,6 @@ function graph() {
     var sampleNames = data.names;
     console.log(data);
     var samples = data.samples;
-<<<<<<< HEAD
-    //appending id values to dropdown bar
-    // sampleNames.forEach((sample) => {
-    //   dropdown
-    //     // selects from index.html 
-    //     .append("option")
-    //     //loads id value
-    //     .text(sample)
-    //     // displays interactive dropdown bar
-    //     .property("value", sample);
-    // })
-    // returns ID value from original data object
-=======
-    
->>>>>>> afb13885d40b279dda53a03dac5bef48df8c184a
     var current_sample = dropdown.node().value;
     
     // returns an object
